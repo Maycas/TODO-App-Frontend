7bb8f39ad419d60e49f5cf0431e7698ea0285b24
Marc Maycas <marc.maycas@gmail.com>
2023-04-29 20:04:17 +0200

    TaskList to get all tasks

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,17 +1,44 @@
+import axios from 'axios'
 import TaskCard from './TaskCard/TaskCard'
+import { useEffect, useState } from 'react'
 
 function TaskList() {
+  const [taskList, setTaskList] = useState([])
+
+  useEffect(() => {
+    tasksGetter()
+  }, [])
+
+  const tasksGetter = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:5001/tasks')
+      setTaskList(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const editButtonHandler = () => {
+    console.log('edit clicked')
+  }
+
+  const deleteButtonHandler = () => {
+    console.log('delete clicked')
+  }
 
   return (
     <>
       <h2>My Tasks</h2>
-      <TaskCard
-        title="Coming home early"
-        dueDate="2023/02/14 23:32"
-        status='COMPLETED'
-        onEdit={() => console.log('edit')}
-        onDelete={() => console.log('delete')}
-      />
+      {taskList.map(task => (
+        <TaskCard
+          key={task.id}
+          title={task.title}
+          dueDate={task.dueDate}
+          status={task.status}
+          onEdit={editButtonHandler}
+          onDelete={deleteButtonHandler}
+        />
+      ))}
     </>
   )
 }
